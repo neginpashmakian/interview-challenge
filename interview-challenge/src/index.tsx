b849162a6745979ee480bf0fa459051fdd146591
neginpashmakian <neginpashmakian6@gmail.com>
2022-07-31 16:44:08 +0430

    9- react query configuration added to the main file

--- interview-challenge/src/index.tsx
+++ interview-challenge/src/index.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import "./index.css";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 

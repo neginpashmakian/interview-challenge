47d0c2978b0e79ed294c25d110446675e65f0aa2
neginpashmakian <neginpashmakian6@gmail.com>
2022-08-04 16:55:47 +0430

    add recoil config to the app file

--- interview-challenge/src/App.tsx
+++ interview-challenge/src/App.tsx
@@ -1,9 +1,22 @@
 import { useRoutes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { RecoilRoot } from 'recoil'
+import RecoilNexus from 'recoil-nexus'
 import { appRoutes } from 'routes'
 import './core/styles/globals.css'
 function App() {
   const element = useRoutes(appRoutes)
-  return <div className="App">{element}</div>
+  return (
+    <div className="App">
+      <ToastContainer />
+      <RecoilRoot>
+        <RecoilNexus />
+        {/* <AuthInitializer /> */}
+        {element}
+      </RecoilRoot>
+    </div>
+  )
 }
 
 export default App
